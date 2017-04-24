d1086c530cd95aea54721c429e8099871544096d
spohngellert-o <spohngellert.o@husky.neu.edu>
2017-04-23 20:26:51 -0700

    Began react test maker.

---
+++ src/index.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import ReactDOM from 'react-dom';;
+import './index.css';
+
+
+class Test extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+      IDCounter: 1,
+			questions: [
+        {
+          question: "What is 2 + 2?",
+          answers: [
+            {answer: "3",
+            answerID: 0},
+            {answer: "4",
+            answerID: 1},
+            {answer: "5",
+            answerID: 2}
+          ],
+          correctAnswer: 1,
+          questionID: 0,
+          IDCounter: 3
+        }
+			]
+		};
+
+    this.addAnswer = this.addAnswer.bind(this);
+	}
+
+  addAnswer(questionIndex) {
+    var questions = this.state.questions.slice();
+    var currQuestion = questions[questionIndex];
+    currQuestion.answers.concat({answer: "", answerID: currQuestion.IDCounter});
+    currQuestion.IDCounter = currQuestion.IDCounter + 1;
+    this.setState(currState => {
+      questions: questions;
+    })
+  }
+
+  renderQuestion(question, index) {
+    return <Question 
+    question={question.question} 
+    answers={question.answers} 
+    correctAnswer={question.correctAnswer}
+    questionID={question.questionID}
+    addAnswerFunc={() => this.addAnswer(index)}/>;
+  }
+
+  render() {
+    const questions = this.state.questions;
+    return (
+      <div>
+        <h1>Test Title</h1>
+        <ol id="test-start">
+          {questions.map((question, index) => <li key={question.questionID}> {this.renderQuestion(question, index)} </li>)}
+        </ol>
+      </div>
+    );
+  }
+
+}
+
+class Question extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        question: props.question,
+        answers: props.answers,
+        correctAnswer: props.correctAnswer,
+        questionID: props.questionID
+    }
+  }
+
+  render() {
+    const answers = this.state.answers;
+    const question = this.state.question;
+    return (
+      <div>
+        <h3>{question}</h3>
+        <ol type="a">
+          {answers.map((answer, index) => <li key={answer.answerID}>{answer.answer}</li>)}
+        </ol>
+        <button onClick={() => this.props.addAnswerFunc()}>
+          Add Answer
+        </button>
+      </div>
+    );
+  }
+}
+ReactDOM.render(
+  <Test/>,
+  document.getElementById('root')
+);
