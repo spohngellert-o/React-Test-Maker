42260e2609f345810ad77f9f19ae091ee713e464
spohngellert-o <spohngellert.o@husky.neu.edu>
2017-06-07 21:46:11 -0700

    AGenericism.

--- src/index.js
+++ src/index.js
@@ -8,26 +8,36 @@ class Test extends React.Component {
 		super();
 		this.state = {
       IDCounter: 1,
-			questions: [
-        {
-          question: "What is 2 + 2?",
-          answers: [
-            {answer: "3",
-            answerID: 0},
-            {answer: "4",
-            answerID: 1},
-            {answer: "5",
-            answerID: 2}
-          ],
-          correctAnswer: 1,
-          questionID: 0,
-          IDCounter: 3
-        }
-			]
-		};
+      title: "",
+      questions: [
+        this.getSampleQuestion(0)
+      ]
+    };
 
     this.addAnswer = this.addAnswer.bind(this);
-	}
+    this.addQuestion = this.addQuestion.bind(this);
+    this.changeQuestion = this.changeQuestion.bind(this);
+  }
+
+  getSampleQuestion(id) {
+    return {
+      question: "",
+      answers: [],
+      correctAnswer: -1,
+      questionID: id,
+      IDCounter: 0
+    };
+  }
+
+  addQuestion() {
+    var currQuestions = this.state.questions.slice();
+    const id = this.state.IDCounter;
+    currQuestions = currQuestions.concat(this.getSampleQuestion(id));
+    this.setState({
+      questions: currQuestions,
+      IDCounter: id + 1
+    });
+  }
 
   addAnswer(questionIndex) {
     var currQuestions = this.state.questions.slice();
@@ -39,25 +49,69 @@ class Test extends React.Component {
     });
   }
 
+  changeQuestion(event, index) {
+    var currQuestions = this.state.questions.slice();
+    var currQuestion = currQuestions[index];
+    currQuestion.question = event.target.value;
+    this.setState({
+      questions: currQuestions
+    });
+  }
+
+  changeAnswer(event, questionIndex, answerIndex) {
+    var currQuestions = this.state.questions.slice();
+    var currQuestion = currQuestions[questionIndex];
+    var currAnswers = currQuestion.answers;
+    currAnswers[answerIndex].answer = event.target.value;
+    this.setState({
+      questions: currQuestions
+    });
+  }
+
+  deleteQuestion(questionIndex) {
+    var currQuestions = this.state.questions.slice();
+    currQuestions.pop(questionIndex);
+    this.setState({
+      questions: currQuestions
+    });
+  }
+
+  deleteAnswer(questionIndex, answerIndex) {
+    var currQuestions = this.state.questions.slice();
+    var currQuestion = currQuestions[questionIndex];
+    var currAnswers = currQuestion.answers;
+    currAnswers.pop(answerIndex);
+    this.setState({
+      questions: currQuestions
+    });
+  }
+
   renderQuestion(question, index) {
     return <Question 
     question={question.question} 
     answers={question.answers} 
     correctAnswer={question.correctAnswer}
     questionID={question.questionID}
-    addAnswerFunc={() => this.addAnswer(index)}/>;
+    addAnswerFunc={() => this.addAnswer(index)}
+    changeAnswerFunc={(event, answerIndex) => this.changeAnswer(event, index, answerIndex)}
+    changeQuestionFunc={(event) => this.changeQuestion(event, index)}
+    deleteQuestionFunc={() => this.deleteQuestion(index)}
+    deleteAnswerFunc={(answerIndex) => this.deleteAnswer(index, answerIndex)}/>;
   }
 
   render() {
     const questions = this.state.questions;
     return (
       <div>
-        <h1>Test Title</h1>
+        <input type="text" onChange={(event) => this.setState({title: event.target.value})} value={this.state.title}/>
         <ol id="test-start">
           {questions.map((question, index) => <li key={question.questionID}> {this.renderQuestion(question, index)} </li>)}
         </ol>
+        <button onClick={() => this.addQuestion()}>
+          Add Question
+        </button>
       </div>
-    );
+      );
   }
 
 }
@@ -69,18 +123,22 @@ class Question extends React.Component {
     const question = this.props.question;
     return (
       <div>
-        <h3>{question}</h3>
-        <ol type="a">
-          {answers.map((answer, index) => <li key={answer.answerID}>{answer.answer}</li>)}
-        </ol>
-        <button onClick={() => this.props.addAnswerFunc()}>
-          Add Answer
-        </button>
-      </div>
+      <input type="text" onChange={this.props.changeQuestionFunc} value={question}/>
+      <ol type="a">
+      {answers.map((answer, index) => 
+        <li key={answer.answerID}>
+        <input type="text" onChange={(event) => this.props.changeAnswerFunc(event, index)} value={answer.answer}/>
+        <button onClick={() => this.props.deleteAnswerFunc(index)}>Delete Answer</button>
+        </li>)
+    }
+    </ol>
+    <button onClick={() => this.props.addAnswerFunc()}>Add Answer</button>
+    <button onClick={() => this.props.deleteQuestionFunc()}>Delete Question</button>
+    </div>
     );
   }
 }
 ReactDOM.render(
   <Test/>,
   document.getElementById('root')
-);
+  );
